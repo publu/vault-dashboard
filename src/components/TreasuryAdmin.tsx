d013620a32a65faa84603a2c4e63288cf0abd71c
Mark Aiken <gwmaiken@att.net>
2022-10-03 18:59:32 -0700

    functional editable list

--- src/components/TreasuryAdmin.tsx
+++ src/components/TreasuryAdmin.tsx
@@ -1,47 +1,51 @@
-import { Web3Provider } from "@ethersproject/providers";
-import Safe from "@gnosis.pm/safe-core-sdk";
 import { MetaTransactionData } from "@gnosis.pm/safe-core-sdk-types";
-import EthersAdapter from "@gnosis.pm/safe-ethers-lib";
-import SafeServiceClient from "@gnosis.pm/safe-service-client";
-import { ethers } from "ethers";
-import _ from "lodash";
-import React, { useEffect, useState } from "react";
-import { useGetList } from "react-admin";
-import { useAccount, useChainId, useProvider } from "../Connectors/Metamask";
+import * as MUI from "@mui/material";
+import { Erc20Stablecoin } from "@qidao/sdk";
+import React, { Dispatch, useEffect, useState } from "react";
+import {
+  Datagrid,
+  ListContextProvider,
+  TextField,
+  TextFieldProps,
+  useGetList,
+  useList,
+  useRecordContext,
+} from "react-admin";
+import { useChainId, useProvider } from "../Connectors/Metamask";
 import { RAVaultInfoAnyVersion } from "../types";
 
 // const safeAddress = "0x3182E6856c3B59C39114416075770Ec9DC9Ff436"; //ETH Address
 // const transactionServiceUrl = "https://safe-transaction.gnosis.io/"; // on rinkeby testnet
 // const chainId = 1;
-const safeAddress = "0xBdeEf118d161ac657AF5Abc2a26487DD894868c7"; //ETH Address
-const transactionServiceUrl = "https://safe-transaction.polygon.gnosis.io/"; // on rinkeby testnet
+// const safeAddress = "0xBdeEf118d161ac657AF5Abc2a26487DD894868c7"; //ETH Address
+// const transactionServiceUrl = "https://safe-transaction.polygon.gnosis.io/"; // on rinkeby testnet
 // const chainId = 137;
 
-const setupSafe = async (metamaskProvider: Web3Provider) => {
-  const ethAdapter = new EthersAdapter({
-    ethers,
-    signer: metamaskProvider.getSigner(),
-  });
-
-  console.log({ chainId: await ethAdapter.getChainId() });
-  const safeService = new SafeServiceClient({
-    ethAdapter,
-    txServiceUrl: transactionServiceUrl,
-  });
-  const safeSdk = await Safe.create({
-    ethAdapter,
-    safeAddress,
-    contractNetworks: {
-      "137": {
-        multiSendAddress: "0xA238CBeb142c10Ef7Ad8442C6D1f9E89e07e7761",
-        multiSendCallOnlyAddress: "0x40A2aCCbd92BCA938b02010E17A5b8929b49130D",
-        safeMasterCopyAddress: "0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552",
-        safeProxyFactoryAddress: "0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2",
-      },
-    },
-  });
-  return { safeService, safeSdk };
-};
+// const setupSafe = async (metamaskProvider: Web3Provider) => {
+//   const ethAdapter = new EthersAdapter({
+//     ethers,
+//     signer: metamaskProvider.getSigner(),
+//   });
+//
+//   console.log({ chainId: await ethAdapter.getChainId() });
+//   const safeService = new SafeServiceClient({
+//     ethAdapter,
+//     txServiceUrl: transactionServiceUrl,
+//   });
+//   const safeSdk = await Safe.create({
+//     ethAdapter,
+//     safeAddress,
+//     contractNetworks: {
+//       "137": {
+//         multiSendAddress: "0xA238CBeb142c10Ef7Ad8442C6D1f9E89e07e7761",
+//         multiSendCallOnlyAddress: "0x40A2aCCbd92BCA938b02010E17A5b8929b49130D",
+//         safeMasterCopyAddress: "0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552",
+//         safeProxyFactoryAddress: "0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2",
+//       },
+//     },
+//   });
+//   return { safeService, safeSdk };
+// };
 
 const saveTemplateAsFile = (filename: string, dataObjToWrite: Object) => {
   const blob = new Blob([JSON.stringify(dataObjToWrite)], {
@@ -63,6 +67,43 @@ const saveTemplateAsFile = (filename: string, dataObjToWrite: Object) => {
   link.remove();
 };
 
+const EditiableRow = (
+  props: TextFieldProps & {
+    vaults: RAVaultInfoAnyVersion[];
+    setVaults: Dispatch<
+      React.SetStateAction<RAVaultInfoAnyVersion[] | undefined>
+    >;
+  }
+) => {
+  const [editMode, setEditMode] = useState(false);
+  const foo: RAVaultInfoAnyVersion = useRecordContext(props);
+  const [collateralValue, setCollateralValue] = useState(foo.collateral);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newInt = parseFloat(event.target.value);
+    const updatedVault = { ...foo, collateral: newInt ? newInt : 0 };
+    const updatedVaults = props.vaults.map((v) =>
+      v.id !== updatedVault.id ? v : updatedVault
+    );
+    console.log({ updatedVaults });
+    props.setVaults(updatedVaults);
+    setCollateralValue(newInt ? newInt : 0);
+  };
+  return (
+    <>
+      {editMode ? (
+        <MUI.TextField
+          value={collateralValue}
+          size="small"
+          onChange={handleChange}
+          onBlur={() => setEditMode(!editMode)}
+        />
+      ) : (
+        <TextField onClick={() => setEditMode(!editMode)} source="collateral" />
+      )}
+    </>
+  );
+};
+
 const TreasuryAdmin = () => {
   const {
     data,
@@ -80,86 +121,94 @@ const TreasuryAdmin = () => {
     },
   });
 
-  const [safeSdk, setSafeSdk] = useState<Safe>();
-  const [safeService, setSafeService] = useState<SafeServiceClient>();
+  // const [safeSdk, setSafeSdk] = useState<Safe>();
+  // const [safeService, setSafeService] = useState<SafeServiceClient>();
   let metamaskProvider = useProvider();
-  const address = useAccount();
   const chainId = useChainId();
-
+  const [vaults, setVaults] = useState(data);
   useEffect(() => {
     const a = async () => {
       if (metamaskProvider) {
-        const { safeService, safeSdk } = await setupSafe(metamaskProvider);
-        setSafeSdk(safeSdk);
-        setSafeService(safeService);
+        // const { safeService, safeSdk } = await setupSafe(metamaskProvider);
+        // setSafeSdk(safeSdk);
+        // setSafeService(safeService);
       }
     };
     void a();
   }, [metamaskProvider]);
 
-  const vaults = data?.map((x) => _.omit(x, ["contractAbi", "contract"]));
   const a = async () => {
-    if (safeSdk) {
-      const vaultWithdrawTxs:
-        | Promise<(MetaTransactionData | undefined) | undefined>[]
-        | undefined = vaults?.map(async (vault) => {
-        if (vault) {
-          const vaultContract = vault.connect(
-            vault.vaultAddress,
-            metamaskProvider
-          );
-
-          console.log({ vaultContract, idx: vault.vaultIdx });
-          try {
-            const collateralAmount = await vaultContract.vaultCollateral(
-              vault.vaultIdx
-            );
-            const foo = vaultContract.populateTransaction.withdrawCollateral(
-              vault.vaultIdx,
-              collateralAmount
-            );
-
-            return {
-              to: vault.vaultAddress,
-              value: "0",
-              data: (await foo).data,
-            };
-          } catch (e) {
-            console.warn({ e });
-            return;
-          }
-        } else return;
-      });
-      if (vaultWithdrawTxs) {
-        const vaultTxs = (await Promise.all(vaultWithdrawTxs)).filter(
-          (item): item is MetaTransactionData => !!item
+    const vaultWithdrawTxs:
+      | Promise<(MetaTransactionData | undefined) | undefined>[]
+      | undefined = vaults?.map(async (vault) => {
+      if (vault && metamaskProvider) {
+        const vaultContract = vault.connect(
+          vault.vaultAddress,
+          metamaskProvider
         );
-        const safeTx = await safeSdk.createTransaction({
-          safeTransactionData: vaultTxs,
-        });
-        console.log({ vaultTxs });
-        console.log({ safeTx });
-        saveTemplateAsFile(`${chainId}-withdraw-txes.json`, vaultTxs);
-        const safeTxHash = await safeSdk.getTransactionHash(safeTx);
-        const signature = await safeSdk.signTransactionHash(safeTxHash);
-        await safeService?.proposeTransaction({
-          safeAddress: safeAddress,
-          senderAddress: address || "",
-          safeTransactionData: safeTx.data,
-          senderSignature: signature.data,
-          safeTxHash: safeTxHash,
-        });
-      }
+
+        try {
+          const collateralAmount = await vaultContract.vaultCollateral(
+            vault.vaultIdx
+          );
+          const foo = await (
+            vaultContract as Erc20Stablecoin
+          ).populateTransaction.withdrawCollateral(
+            vault.vaultIdx,
+            collateralAmount
+          );
+          // const foo = vaultContract.populateTransaction.withdrawCollateral( );
+
+          return {
+            to: vault.vaultAddress,
+            value: "0",
+            data: foo.data || "",
+          };
+        } catch (e) {
+          console.warn({ e });
+          return;
+        }
+      } else return;
+    });
+    if (vaultWithdrawTxs) {
+      const vaultTxs = (await Promise.all(vaultWithdrawTxs)).filter(
+        (item): item is MetaTransactionData => !!item
+      );
+      // const safeTx = await safeSdk.createTransaction({
+      //   safeTransactionData: vaultTxs,
+      // });
+      console.log({ vaultTxs });
+      // console.log({ safeTx });
+      saveTemplateAsFile(`${chainId}-withdraw-txes.json`, vaultTxs);
+      // const safeTxHash = await safeSdk.getTransactionHash(safeTx);
+      // const signature = await safeSdk.signTransactionHash(safeTxHash);
+      // await safeService?.proposeTransaction({
+      //   safeAddress: safeAddress,
+      //   senderAddress: address || "",
+      //   safeTransactionData: safeTx.data,
+      //   senderSignature: signature.data,
+      //   safeTxHash: safeTxHash,
+      // });
     }
   };
   // }, [address, metamaskProvider, safeSdk, safeService, vaults]);
-
+  const listContext = useList({ data: vaults || [] });
   return (
     <div>
       {isLoading ? (
         <div>Loading...</div>
       ) : (
-        <button onClick={() => a()}>Click me to sign</button>
+        <>
+          <button onClick={() => a()}>Click me to sign</button>
+          <ListContextProvider value={listContext}>
+            <Datagrid>
+              <TextField source="vaultIdx" />
+              <TextField source="chainId" />
+              <EditiableRow vaults={vaults || []} setVaults={setVaults} />
+              <TextField source="token.name" />
+            </Datagrid>
+          </ListContextProvider>
+        </>
       )}
     </div>
   );
