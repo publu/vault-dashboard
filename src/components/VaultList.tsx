425c49c3698ee3f0acd03d30f5db5bbed61f9e8e
Mark Aiken <gwmaiken@att.net>
2022-10-06 19:05:31 -0700

    stabilize vault multicall fetching

--- src/components/VaultList.tsx
+++ src/components/VaultList.tsx
@@ -1,37 +1,54 @@
 import React from "react";
-import {Datagrid, List, NumberField, TextField} from "react-admin";
+import {
+  Datagrid,
+  ExportButton,
+  FilterButton,
+  List,
+  NumberField,
+  TextField,
+  TopToolbar,
+} from "react-admin";
+import DashPagination from "./DashPagination";
+import { DeepLinkField } from "./DeepLinkField";
 import LoadingDisplay from "./LoadingDisplay";
+import { NetworkButton } from "./MetaMaskConnectButton";
 import searchFilters from "./Search";
-import DashPagination from "./DashPagination"
-import {TopToolbar, FilterButton, ExportButton} from "react-admin";
-import {NetworkButton} from "./MetaMaskConnectButton";
-import LiquidateButton from './LiquidateButton'
-import {DeepLinkField} from "./DeepLinkField";
 
 const ListActions = () => (
-    <TopToolbar>
-        <FilterButton/>
-        <ExportButton/>
-        <NetworkButton title={"Metamask"}/>
-    </TopToolbar>
+  <TopToolbar>
+    <FilterButton />
+    <ExportButton />
+    <NetworkButton title={"Metamask"} />
+  </TopToolbar>
 );
 
 const VaultList: React.FC = () => {
-    return (<List actions={<ListActions/>} perPage={100} pagination={<DashPagination />} empty={<LoadingDisplay/>}
-                  queryOptions={{refetchInterval: 1000}} filters={searchFilters}>
-        <Datagrid>
-            <TextField source="vaultChainName"/>
-            <TextField source="tokenName"/>
-            <NumberField source="vaultIdx"/>
-            <TextField source="owner"/>
-            <NumberField source="collateral" options={{style: 'decimal'}}/>
-            <NumberField source="debt" options={{ style: 'currency', currency: 'USD' }}/>
-            <NumberField source="cdr" options={{style: 'percent'}}/>
-            <NumberField source="risky"/>
-            <DeepLinkField source="vaultLink"/>
-            <LiquidateButton/>
-        </Datagrid>
-    </List>)
-}
+  return (
+    <List
+      actions={<ListActions />}
+      perPage={100}
+      pagination={<DashPagination />}
+      empty={<LoadingDisplay />}
+      queryOptions={{ refetchInterval: 1000 }}
+      filters={searchFilters}
+    >
+      <Datagrid>
+        <TextField source="vaultChainName" />
+        <TextField source="tokenName" />
+        <NumberField source="vaultIdx" />
+        <TextField source="owner" />
+        <NumberField source="collateral" options={{ style: "decimal" }} />
+        <NumberField
+          source="debt"
+          options={{ style: "currency", currency: "USD" }}
+        />
+        <NumberField source="cdr" options={{ style: "percent" }} />
+        <NumberField source="risky" />
+        <DeepLinkField source="vaultLink" />
+        {/*<LiquidateButton/>*/}
+      </Datagrid>
+    </List>
+  );
+};
 
-export default VaultList
+export default VaultList;
--- src/multicall.ts
+++ src/multicall.ts
@@ -1,17 +1,22 @@
 import { ChainId } from "@qidao/sdk";
 import { Provider, setMulticallAddress } from "ethers-multicall";
+import _ from "lodash";
+import PQueue from "p-queue";
 import { MULTICALL_NETWORKS, PROVIDERS, RPCS } from "./constants";
 
 const ethcallProvider: { [chainId in ChainId]?: Provider } = {};
+const ethcallQueue: { [chainId in ChainId]?: PQueue } = {};
+const MULTICALL_CONCURRENCY = 5;
 
 async function setupAndInitMulticall(chainId: ChainId) {
   setMulticallAddress(chainId, MULTICALL_NETWORKS[chainId] || "");
-  console.log(`Setting up multicall for Chain ${chainId}`)
+  console.log(`Setting up multicall for Chain ${chainId}`);
   let provider = PROVIDERS[chainId];
   if (provider) {
     const callProvider = new Provider(provider);
     await callProvider.init();
     ethcallProvider[chainId] = callProvider;
+    ethcallQueue[chainId] = new PQueue({ concurrency: MULTICALL_CONCURRENCY });
   }
 }
 
@@ -27,8 +32,15 @@ export async function init() {
 
 export async function multicall(chainId: ChainId, calls: any[]) {
   const callProvider = ethcallProvider[chainId];
+  const queue = ethcallQueue[chainId];
   if (callProvider) {
-    return await callProvider.all(calls);
+    return (
+      await Promise.all(
+        _.chunk(calls, 450).map((cs) => {
+          return queue?.add(() => callProvider.all(cs));
+        })
+      )
+    ).flat();
   } else {
     throw new Error(
       `No call provider for chain: ${chainId}, did you call init?`
