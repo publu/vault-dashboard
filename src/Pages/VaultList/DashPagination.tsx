b3ed71031ce46934cfa12ef0da0de31e8ac541c4
Mark Aiken <2439803+royalaid@users.noreply.github.com>
2022-11-27 13:54:13 -0500

    clean up pagination component, fix title bar

--- src/Pages/VaultList/DashPagination.tsx
+++
@@ -1,8 +0,0 @@
-import { Pagination, PaginationProps } from "react-admin";
-
-const DashPagination = (props: PaginationProps) => {
-  // const perPage = 100
-  return <Pagination rowsPerPageOptions={[25, 100, 200, 500]} {...props} />;
-};
-
-export default DashPagination;
