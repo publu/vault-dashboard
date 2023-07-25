dfed923430debc4324361b69d7c8870291282295
Royalaid <2439803+royalaid@users.noreply.github.com>
2023-07-25 14:50:51 -0700

    add tailwind

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,7 +3,7 @@ import { MyMenu } from './Menu'
 import { MyAppBar } from './MyAppBar'
 
 export const Layout = (props: any) => {
-    return <RA.Layout {...props} menu={MyMenu} appBar={MyAppBar} />
+    return <RA.Layout {...props} children={<div className="mt-3"> {props.children} </div>} menu={MyMenu} appBar={MyAppBar} />
 }
 
 export default Layout
